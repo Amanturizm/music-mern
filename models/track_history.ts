212b0e7dcf7448e9822f36ee501182b0607aafbb
Amantur Momunjanov <amanturmomunjanov12580@gmail.com>
2023-09-07 15:22:04 +0600

    add validate in models/track_history.ts

--- models/track_history.ts
+++ models/track_history.ts
@@ -1,15 +1,25 @@
 import { model, Schema, Types } from 'mongoose';
+import User from './user';
+import Track from './track';
 
 const TrackHistorySchema = new Schema({
   user: {
     type: Types.ObjectId,
     ref: 'User',
     required: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => User.findById(value),
+      message: 'User does not exist!',
+    },
   },
   track: {
     type: Types.ObjectId,
     ref: 'Track',
     required: true,
+    validate: {
+      validator: async (value: Types.ObjectId) => Track.findById(value),
+      message: 'Track does not exist!',
+    },
   },
   datetime: {
     type: Date,
